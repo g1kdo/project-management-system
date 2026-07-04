6b1504e7aceb11e47ea46dcc21a82b948ce8e04a
g1kdo <july.gassy-8k@icloud.com>
2026-07-05 01:42:19 +0200

    Implemented utils for validation, better clean codes and reusability

---
+++ src/utils/ConsoleMenu.java
@@ -0,0 +1,55 @@
+package utils;
+
+import models.project.Project;
+import models.user.User;
+import services.ProjectService;
+import services.ReportService;
+import services.TaskService;
+
+public class ConsoleMenu {
+    public static void printMainMenu(User currentUser) {
+        System.out.println("\n╔════════════════════════════════════════════╗");
+        System.out.println("║     JAVA PROJECT MANAGEMENT SYSTEM         ║");
+        System.out.println("╚════════════════════════════════════════════╝");
+        System.out.printf("Current User: %s (%s)%n%n", currentUser.getUserName(), currentUser.getRole());
+        System.out.println("Main Menu:");
+        System.out.println("-----------");
+        System.out.println("1. Manage Projects");
+        System.out.println("2. Manage Tasks");
+        System.out.println("3. View Status Reports");
+        System.out.println("4. Switch User");
+        System.out.println("5. Exit");
+    }
+
+    public  static void printProjectMenu(ProjectService service) {
+        System.out.println("\n╔════════════════════════════════════════════╗");
+        System.out.println("║               PROJECT CATALOG              ║");
+        System.out.println("╚════════════════════════════════════════════╝");
+        System.out.println("Filter Options:");
+        System.out.printf("1. View All Projects (%d)%n", service.getProjectCount());
+        System.out.println("2. Software Projects Only");
+        System.out.println("3. Hardware Projects Only");
+        System.out.println("4. Search by Budget Range");
+        System.out.println("5. Add New Project");
+        System.out.println("6. Back to Main Menu");
+    }
+
+    public static void printDetailsMenu(Project project, TaskService taskService, ReportService reportService) {
+        System.out.println("\n╔════════════════════════════════════════════╗");
+        System.out.printf("║           PROJECT DETAILS: %-15s║%n", project.getProjectID());
+        System.out.println("╚════════════════════════════════════════════╝");
+        System.out.println("Project Name: " + project.getProjectName());
+        System.out.println("Type: " + project.getProjectDetails());
+        System.out.println("Team Size: " + project.getTeamSize());
+        System.out.printf("Budget: Rwf%,.2f%n", project.getBudget());
+        System.out.println("\nAssociated Tasks:");
+        taskService.viewTasksForProject(project);
+        System.out.printf("Completion Rate: %.2f%%%n%n", reportService.calculateProjectCompletionRate(project));
+
+        System.out.println("Options:");
+        System.out.println("1. Add New Task");
+        System.out.println("2. Update Task Status");
+        System.out.println("3. Remove Task");
+        System.out.println("4. Back to Catalog");
+    }
+}
---
+++ src/utils/ValidationUtils.java
@@ -0,0 +1,42 @@
+package utils;
+
+import models.task.Task;
+
+import java.util.Scanner;
+
+public class ValidationUtils {
+    public static int getValidInt(Scanner scan, String prompt, int min, int max) {
+        while (true) {
+            System.out.print(prompt);
+            String userInput = scan.nextLine().trim();
+            try {
+                int value = Integer.parseInt(userInput);
+                if (value >= min && value <= max) {
+                    return value;
+                }
+                System.out.printf("Invalid choice: Please enter a number between %d and %d.%n", min, max);
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Invalid numeric input:(");
+            }
+
+        }
+    }
+
+    public static double getValidDouble(Scanner scan, String prompt, double min) {
+        while (true) {
+            System.out.print(prompt);
+            String userInput = scan.nextLine().trim();
+            try {
+                double value = Integer.parseInt(userInput);
+                if (value >= min) {
+                    return value;
+                }
+                System.out.printf("Error: Value must be greater than or equal to %.2f.%n", min);
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Invalid numeric input:(");
+            }
+
+        }
+    }
+
+}
