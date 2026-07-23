b4c83fb84b148f2e4336cb21f730bf5f5aa0aba6
g1kdo <july.gassy-8k@icloud.com>
2026-07-23 12:53:48 +0200

    feature: Implement new login-register logic for user authentication

---
+++ src/services/user/UserService.java
@@ -0,0 +1,56 @@
+package services.user;
+
+import models.user.AdminUser;
+import models.user.RegularUser;
+import models.user.User;
+import utils.exceptions.InvalidInputException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserService {
+    private final Map<String, User> userCatalog;
+    private User currentUser;
+
+    public UserService() {
+        this.userCatalog = new HashMap<>();
+        initializeDefaultUsers();
+    }
+
+    private void initializeDefaultUsers() {
+        registerUser(new AdminUser("Katy Great Adonai", "katygreatado@gmail.com", "admin123"));
+        registerUser(new RegularUser("Aline NZIKWINKUNDA", "nzikaline@gmail.com", "user123"));
+    }
+
+    public void registerUser(User user) {
+        if (userCatalog.containsKey(user.getUserName().toLowerCase()))
+            throw new InvalidInputException("Username '\" + user.getUsername() + \"' is already registered.");
+
+        userCatalog.put(user.getUserName().toLowerCase(), user);
+    }
+
+    public User login(String username, String password) {
+        User user = userCatalog.get(username.toLowerCase());
+        if (user == null || !user.verifyPassword(password))
+            throw new InvalidInputException("Invalid username or password.");
+
+        this.currentUser = user;
+        System.out.println("✓ Welcome back, " + user.getUserName() + " [" + user.getRole() + "]!");
+        return user;
+    }
+
+    public void logout() {
+        if (currentUser != null) {
+            System.out.println("✓ User '" + currentUser.getUserName() + "' logged out successfully.");
+            this.currentUser = null;
+        }
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public Map<String, User> getAllUsers() {
+        return userCatalog;
+    }
+}
